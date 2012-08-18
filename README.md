1e2ebf3d671392e0e3e28252afe1473e8bd8c21a
Nikolay <tnimas@gmail.com>
2012-08-18 23:27:50 +0400

    add readme and license file. Code moved in src folder.

---
+++ README.md
@@ -0,0 +1,5 @@
+Tic Tac Toe
+====================
+Russian interface. You can only play with 2 different network connections.
+maybe it's tic-tac-toe with the most sophisticated architecture code.
+see tnimas.com (russian) for details.
---
+++ src/UIGameTTT.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    partial class UIGameTTT
+    {
+
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Background = new System.Windows.Forms.PictureBox();
+            this.appMenu = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).BeginInit();
+            this.appMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Background
+            // 
+            this.Background.Location = new System.Drawing.Point(12, 27);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(768, 538);
+            this.Background.TabIndex = 0;
+            this.Background.TabStop = false;
+            this.Background.Paint += new System.Windows.Forms.PaintEventHandler(this.BackgroundPaint);
+            this.Background.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BackgroundMouseClick);
+            // 
+            // appMenu
+            // 
+            this.appMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.appMenu.Location = new System.Drawing.Point(0, 0);
+            this.appMenu.Name = "appMenu";
+            this.appMenu.Size = new System.Drawing.Size(782, 28);
+            this.appMenu.TabIndex = 1;
+            this.appMenu.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.menuToolStripMenuItem.Text = "Меню";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(158, 24);
+            this.newGameToolStripMenuItem.Text = "Новая игра";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.NewGameToolStripMenuItemClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(158, 24);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
+            // 
+            // UIGameTTT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 568);
+            this.Controls.Add(this.Background);
+            this.Controls.Add(this.appMenu);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.MainMenuStrip = this.appMenu;
+            this.Name = "UIGameTTT";
+            this.Text = "Крестики-нолики";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UIGameTTTFormClosing);
+            this.Load += new System.EventHandler(this.TTTFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).EndInit();
+            this.appMenu.ResumeLayout(false);
+            this.appMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Background;
+        private System.Windows.Forms.MenuStrip appMenu;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
